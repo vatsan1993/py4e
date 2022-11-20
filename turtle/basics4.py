b49a8d2ae2d4dfec2d0c132723800837c4315aa4
Srivatsan <vatsan1993@gmail.com>
2022-11-20 17:14:39 +0530

    concentric squares

---
+++ turtle/basics4.py
@@ -0,0 +1,40 @@
+import turtle
+# getting tutle window
+wn= turtle.Screen()
+wn.bgcolor('light green')
+wn.title('Simple Drawing')
+
+# creating turtle
+t= turtle.Turtle()
+# outside to inside
+def square(size):
+    for _ in range(4):
+        t.forward(size)
+        t.right(90)
+        size-=5
+# inside to outside
+def sqrfunc(size):
+    for i in range(4):
+        t.fd(size)
+        t.left(90)
+        size = size + 5
+
+square(146)
+square(126)
+square(106)
+square(86)
+square(66)
+square(46)
+square(26)
+
+
+
+sqrfunc(6)
+sqrfunc(26)
+sqrfunc(46)
+sqrfunc(66)
+sqrfunc(86)
+sqrfunc(106)
+sqrfunc(126)
+sqrfunc(146)
+turtle.done()
